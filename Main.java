39bc72532c8e0ec9496d6201dbeecb0caa706c6e
presly808 <presly808@gmail.com>
2015-10-02 22:03:48 +0300

    first

---
+++ Main.java
@@ -0,0 +1,9 @@
+// set of methods
+class Main {
+
+   // method, sequence of actions
+   public static void main(String[] args){ 
+      System.out.println("Hello world"); // instruction to processor
+   }	
+
+}
